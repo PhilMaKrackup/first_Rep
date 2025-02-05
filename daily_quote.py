bf47727e91e5ae11872d2811378d3b0a0efd60d9
Cody Hartsock <codyhartsock28@gmail.com>
2025-02-04 23:31:52 -0700

    Moved daily_quote.py to root directory

--- daily_quote.py
+++ daily_quote.py
@@ -9,18 +9,4 @@ quotes = [
 
 print(random.choice(quotes))
 
-name: Daily Motivation
 
-on:
-  schedule:
-    - cron: '0 9 * * *'  # Runs every day at 9 AM UTC
-
-jobs:
-  run-quote:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout Repository
-        uses: actions/checkout@v3
-      
-      - name: Run Quote Script
-        run: python daily_quote.py
